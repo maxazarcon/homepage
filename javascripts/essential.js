ee2f13f015c55209fa425bd0fa5c54036a881d3e
Max Azarcon <max@trinitysedge.com>
2015-04-18 12:20:37 -0400

    testing fades

---
+++ javascripts/essential.js
@@ -0,0 +1,72 @@
+$(function() {
+
+	$("#homeButton").on("click", function() {
+		$("#homeTab").show();
+		$("#contactTab").hide();
+		$("#portfolioTab").hide();
+		$("#skillsTab").hide();
+		$("#resumeTab").hide();
+
+		$("#homeButton").fadeOut();
+		$("#contactButton").show();
+		$("#portfolioButton").show();
+		$("#skillsButton").show();
+		$("#resumeButton").show();
+	});
+
+	$("#contactButton").on("click", function() {
+		$("#homeTab").hide();
+		$("#contactTab").show();
+		$("#portfolioTab").hide();
+		$("#skillsTab").hide();
+		$("#resumeTab").hide();
+
+		$("#homeButton").show();
+		$("#contactButton").fadeOut();
+		$("#portfolioButton").show();
+		$("#skillsButton").show();
+		$("#resumeButton").show();
+	});
+
+	$("#portfolioButton").on("click", function() {
+		$("#homeTab").hide();
+		$("#contactTab").hide();
+		$("#portfolioTab").show();
+		$("#skillsTab").hide();
+		$("#resumeTab").hide();
+
+		$("#homeButton").show();
+		$("#contactButton").show();
+		$("#portfolioButton").fadeout();
+		$("#skillsButton").show();
+		$("#resumeButton").show();
+	});
+	
+		$("#skillsButton").on("click", function() {
+		$("#homeTab").hide();
+		$("#contactTab").hide();
+		$("#portfolioTab").hide();
+		$("#skillsTab").show();
+		$("#resumeTab").hide();
+
+		$("#homeButton").show();
+		$("#contactButton").show();
+		$("#portfolioButton").show();
+		$("#skillsButton").fadeOut();
+		$("#resumeButton").show();
+		
+		$("#resumeButton").on("click", function() {
+		$("#homeTab").hide();
+		$("#contactTab").hide();
+		$("#portfolioTab").hide();
+		$("#skillsTab").hide();
+		$("#resumeTab").show();
+
+		$("#homeButton").show();
+		$("#contactButton").show();
+		$("#portfolioButton").show();
+		$("#skillsButton").show();
+		$("#resumeButton").fadeOut();
+	});
+	});
+});
